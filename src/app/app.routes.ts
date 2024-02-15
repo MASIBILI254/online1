387873cf1e57c05dc025859082a79a8c871dbce1
MASIBILI254 <andreasimiyu7@gmail.com>
2024-02-15 10:53:54 +0300

    jk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'home','title':'home', component:HomeComponent},
+    {'path':'about','title':'about',component:AboutComponent},
+    {'path':'services','title':'services',component:ServicesComponent}
+];
