1944b1712690aa6b7a1fb97c0cc340599f31a6c8
MASIBILI254 <andreasimiyu7@gmail.com>
2024-02-15 11:12:53 +0300

    kk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
-    {'path': 'home','title':'home', component:HomeComponent},
+    {'path': '','title':'home', component:HomeComponent},
     {'path':'about','title':'about',component:AboutComponent},
     {'path':'services','title':'services',component:ServicesComponent}
 ];
